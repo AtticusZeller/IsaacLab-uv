a03e92279845e0eb9361f84d7da111fb02730fe3
Atticuszz <1831768457@qq.com>
2025-12-08 20:15:20 +0800

    Update isaaclab to latest tag

--- scripts/update_isaaclab.sh
+++ scripts/update_isaaclab.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-git submodule update --remote isaaclab
+echo "Entering isaaclab submodule..."
+cd isaaclab || exit
+
+echo "Fetching tags..."
+git fetch --tags
+
+LATEST_TAG=$(git tag --sort=-v:refname | head -n 1)
+echo "Found latest tag: $LATEST_TAG"
+
+git checkout "$LATEST_TAG"
+
+cd ..
+
+echo "Committing changes to parent repo..."
 
 git add isaaclab
-git commit -m "Update IsaacLab submodule to latest"
+git commit -m "Update isaaclab submodule to tag $LATEST_TAG"
+
+echo "Done! IsaacLab is now at $LATEST_TAG"
